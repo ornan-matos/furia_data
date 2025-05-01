d259cd94879f2f2cf850fc02f53397b15c8897fc
ornan-matos <ornanmatos@outlook.com>
2025-05-01 11:57:55 -0300

    update:ajustar e permitir acesso a area de administração

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import streamlit as st
-from app.callback_handler import processar_callback
-from app.database import inicializar_banco
 from app import fan_form, admin_panel
 from app.config import config
+from app.database import inicializar_banco
+from app.callback_handler import processar_callback
 
-# Deve ser o primeiro comando Streamlit
 st.set_page_config(page_title="FURIA Fan Data", layout="wide")
-
-processar_callback()
-inicializar_banco()
 
 with st.sidebar:
     st.image("static/logo_furia.png", width=180)
@@ -18,12 +14,12 @@ with st.sidebar:
 if menu == "Cadastro de Fã":
     fan_form.render()
 elif menu == "Administração":
-    if not st.session_state.get("admin_autenticado"):
-        senha_input = st.text_input("Senha do Administrador", type="password", key="admin_password")
-        if senha_input == config.ADMIN_PASSWORD:
-            st.session_state["admin_autenticado"] = True
-            st.experimental_rerun()
-        elif senha_input:
-            st.error("Senha incorreta.")
-    else:
+    if st.text_input("Senha do Administrador", type="password") == config.ADMIN_PASSWORD:
         admin_panel.render()
+    else:
+        st.warning("Acesso restrito à administração.")
+
+processar_callback()
+
+inicializar_banco()
+
